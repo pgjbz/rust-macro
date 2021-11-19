89bddabb77711aa9955ce851c75b3b4538cd63a0
Paulo Gabriel <paulo.bezerra.ext@sascar.com.br>
2021-11-19 17:15:15 -0300

    Repetitions

---
+++ repetitions/main.rs
@@ -0,0 +1,29 @@
+macro_rules! vec_strs {
+    (   
+        //start repetition
+        $(
+            $element:expr
+        )
+        , //seperated by commas...
+        * //zero or more times, like Regex
+    ) => {
+        //Enclose the expansion in a block so that we can use
+        //multiple staements.
+        {
+            let mut v: Vec<String> = Vec::new();
+            //start a repetition:
+            $(
+                // Each repeat will contain the follwing statement, with
+                // $element replaced with the corresponding expression
+                v.push(format!("{}", $element));
+            )* 
+            v
+        }
+    };
+
+}
+
+fn main() {
+    let my_vec = vec_strs![1, 3, 2];
+    println!("{:?}", my_vec);
+}
