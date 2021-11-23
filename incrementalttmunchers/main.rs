49f8e9e88bb89be7fcf60d7b4b3adad2cc76965e
Paulo Gabriel <paulo.bezerra.ext@sascar.com.br>
2021-11-23 13:48:12 -0300

    Incremental tt munchers

---
+++ incrementalttmunchers/main.rs
@@ -0,0 +1,22 @@
+macro_rules! mixed_rules {
+    () => {};
+    (trace $name:ident; $($tail:tt)*) => {
+        {
+            println!(concat!(stringify!($name), " = {:?}"), $name);
+            mixed_rules($($tail)*)
+        }
+    };
+    (trace $name:ident = $init:expr; $($tail:tt)*) => {
+        {
+            let $name = $init;
+            println!(concat!(stringify!($name), " = {:?}"), $name);
+            mixed_rules!($($tail)*);
+        }
+    };
+}
+
+fn main() {
+
+    mixed_rules!(trace a = 10;);
+
+}
