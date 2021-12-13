e1da241c88123038f40592b715287f11f2e1b202
Paulo Gabriel <paulo.bezerra.ext@sascar.com.br>
2021-12-13 13:26:05 -0300

    push-down-accumulation

---
+++ push-down-accumlation/main.rs
@@ -0,0 +1,22 @@
+macro_rules! init_array {
+    (@accum (0, $_e:expr) -> ($($body:tt)*))
+        => {init_array!(@as_expr [$($body)*])};
+    (@accum (1, $e:expr) -> ($($body:tt)*))
+        => {init_array!(@accum (0, $e) -> ($($body)* $e,))};
+    (@accum (2, $e:expr) -> ($($body:tt)*))
+        => {init_array!(@accum (1, $e) -> ($($body)* $e,))};
+    (@accum (3, $e:expr) -> ($($body:tt)*))
+        => {init_array!(@accum (2, $e) -> ($($body)* $e,))};
+    (@as_expr $e:expr) => {$e};
+    [$e:expr; $n:tt] => {
+        {
+            let e = $e;
+            init_array!(@accum ($n, e.clone()) -> ())
+        }
+    };
+}
+
+fn main() {
+    let strings: [String; 3] = init_array![String::from("hi!");3];
+    eprintln!("{:?}", strings);
+}
